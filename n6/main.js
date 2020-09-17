1dc32bebeaf7fd00d13ee9a2f6191c12c2cff8ff
Janek Janiak <janek.janiak@gmail.com>
2020-09-17 09:38:45 +0200

    Finish project 6

--- n6/main.js
+++ n6/main.js
@@ -1,22 +1,43 @@
 const btn=document.querySelectorAll('button');
 const image=document.querySelector('.slider');
-const bikes=['b1.jpg', 'b2.jpg', 'b3.jpg'];
+const bikes=['b1.jpg', 'b2.jpg', 'b3.jpg', 'b4.jpg'];
 let active =0;
 
+const load=()=>{
+  currentImg=bikes[active];
+  image.style.backgroundImage = `url(${currentImg})`;
+}
+load();
 
 btn.forEach(function(button){
     button.addEventListener('click', function(e){
       if (button.classList.contains('forward')){
-        currentImg=bikes[active];
-        image.style.backgroundImage = `url(${currentImg})`;
-        active++;
-        if(active === bikes.length){
+        if(active === bikes.length -1){
+          console.log(`if ${active}`);
           active = 0
+          currentImg=bikes[active];
+          image.style.backgroundImage = `url(${currentImg})`;
+        }else{
+          console.log(`else ${active}`);  
+          active++;
+          currentImg=bikes[active];
+          image.style.backgroundImage = `url(${currentImg})`;
+        }
+      }else{
+        if(active===0){
+          console.log(`if back ${active}`);
+          active=bikes.length-1;
+          console.log(`if back po ${active}`)
+          currentImg=bikes[active];
+          image.style.backgroundImage = `url(${currentImg})`;
+        }else{
+          console.log(`else back ${active}`);
+          active--;
+          currentImg=bikes[active];
+          image.style.backgroundImage = `url(${currentImg})`;
         }
 
       }
-      if (button.classList.contains('back')){
-
-      }
-    })
-  });
+    }
+  )
+});   
